51687c034416fdaaf7f5f494b543f359217edcc7
VGotovtseva <gtvcva@mail.ru>
2023-07-09 18:31:13 +0300

    CommitAdd

---
+++ home_work/5_hw.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+def test_site_visit():
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+    try:
+        username = driver.find_element(By.CSS_SELECTOR, '#user-name')
+        password = driver.find_element(By.CSS_SELECTOR, '#password')
+        login_button = driver.find_element(By.CSS_SELECTOR, '#login-button')
+
+    except NoSuchElementException:
+        assert False
+    assert True
+
+    if username != "" and password != "" and login_button != "":
+        print("Элементы найдены")
+
+
--- tests/test_2_open_site.py
+++ tests/test_2_open_site.py
@@ -1,5 +1,42 @@
-from selenium import webdriver
+import time
 
-driver = webdriver.Chrome()
-driver.get("https://demoqa.com/")
-9
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+
+
+#def test_site_visit():
+    #driver = webdriver.Chrome()
+   # driver.get("https://demoqa.com/")
+
+    #try:
+        #driver.find_element(By.CSS_SELECTOR,'header > a > img') #в локаторах всегда идет поиск по последнему элементу
+   ## except NoSuchElementException:
+      #  assert False
+    #assert True
+
+#def test_banner_image():
+    #driver = webdriver.Chrome()
+    #driver.get("https://demoqa.com/")
+
+    #try:
+       ##except NoSuchElementException:
+        #assert False
+    #assert True
+
+
+def test_banner_button():
+    driver = webdriver.Chrome()
+    driver.get("https://demoqa.com/")
+
+    try:
+        button_elements = driver.find_element(By.CSS_SELECTOR, 'div.home-body >div > div:nth-child(1)')
+    except NoSuchElementException:
+        assert False
+    assert True
+
+    button_elements.click()
+    time.sleep(3) #ожидание
+
+    assert driver.current_url == 'https://demoqa.com/elements'
